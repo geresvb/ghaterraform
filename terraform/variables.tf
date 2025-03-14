12a98d4c75dd3fc2ad2c4de50daab0848319a4d4
geresvb <azureuser@RunnerGitHub.4bsxtr5pzoyebnjcqzribevijc.bx.internal.cloudapp.net>
2025-03-14 18:18:09 +0000

    ids

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,4 +5,13 @@ variable "location" {
 variable "vm_size" {
   default = "Standard_B1s"
 }
-#
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
